24375402e538977bc6fc3f17db5a5e8ba6a8085d
Brun0Hen1que <brunoparente22@gmail.com>
2024-06-24 21:50:55 -0300

    fechar camera ao fechar modal

--- camera.js
+++ camera.js
@@ -2,16 +2,6 @@ const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-// // Solicitar permissão para acessar a câmera
-// navigator.mediaDevices.getUserMedia({ video: true })
-//     .then((stream) => {
-//         video.srcObject = stream;
-//         video.play();
-//     })
-//     .catch((err) => {
-//         console.error("Erro ao acessar a câmera: ", err);
-//     });
-
 const cameraBtn = document.getElementById('cameraBtn');
 const cameraModal = document.getElementById('cameraModal');
 const closeCameraModal = document.getElementById('closeCameraModal');
@@ -26,54 +16,23 @@ cameraBtn.onclick = function () {
         .catch((err) => {
             console.error("Erro ao acessar a câmera: ", err);
         });
-    //     const html5QrCode = new Html5Qrcode("reader");
-    //     html5QrCode.start(
-    //         { facingMode: "environment" },
-    //         {
-    //             fps: 10,
-    //             qrbox: 250
-    //         },
-    //         qrCodeMessage => {
-    //             alert(`QR Code detected: ${qrCodeMessage}`);
-    //             html5QrCode.stop().then(ignore => {
-    //                 cameraModal.style.display = "none";
-    //             }).catch(err => {
-    //                 console.log(err);
-    //             });
-    //         },
-    //         errorMessage => {
-    //             console.log(errorMessage);
-    //         })
-    //         .catch(err => {
-    //             console.log(err);
-    //         });
-    // Solicitar permissão para acessar a câmera
-
 }
 
-closeCameraModal.onclick = function () {
-    cameraModal.style.display = "none";
-    //     Html5Qrcode.getCameras().then(cameras => {
-    //         if (cameras.length) {
-    //             const html5QrCode = new Html5Qrcode("reader");
-    //             html5QrCode.stop();
-    //         }
-    //     }).catch(err => {
-    //         console.log(err);
-    //     });
-}
-
-// window.onclick = function (event) {
-//     if (event.target == cameraModal) {
-//         cameraModal.style.display = "none";
-//         Html5Qrcode.getCameras().then(cameras => {
-//             if (cameras.length) {
-//                 const html5QrCode = new Html5Qrcode("reader");
-//                 html5QrCode.stop();
-//             }
-//         }).catch(err => {
-//             console.log(err);
-//         });
-//     }
+// closeCameraModal.onclick = function () {
+//     cameraModal.style.display = "none";
 // }
 
+document.getElementById('closeCameraModal').onclick = function() {
+    document.getElementById('cameraModal').style.display = "none";
+    const video = document.getElementById('video');
+    if (video.srcObject) {
+        const stream = video.srcObject;
+        const tracks = stream.getTracks();
+
+        tracks.forEach(function(track) {
+            track.stop();
+        });
+
+        video.srcObject = null;
+    }
+};
