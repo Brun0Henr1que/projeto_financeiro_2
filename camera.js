a72605dea2b1c64c89b35ef1d7a44ed49a1d9ba9
Brun0Hen1que <brunoparente22@gmail.com>
2024-06-26 00:32:59 -0300

    adicionado mais formas de codificação

--- camera.js
+++ camera.js
@@ -56,7 +56,19 @@ document.getElementById('detect').onclick = function () {
         src: canvas.toDataURL(),
         numOfWorkers: 0,
         decoder: {
-            readers: ["code_128_reader"]
+            readers: [
+                "code_128_reader",
+                "ean_reader",
+                "ean_8_reader",
+                "code_39_reader",
+                "code_39_vin_reader",
+                "codabar_reader",
+                "upc_reader",
+                "upc_e_reader",
+                "i2of5_reader",
+                "2of5_reader",
+                "code_93_reader"
+            ] 
         },
         locate: true,
     }, function(result) {
